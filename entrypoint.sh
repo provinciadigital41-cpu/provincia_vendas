4a4f56c1c58579a73374c7079a8177c20fa62e1e
provinciadigital41-cpu <provinciadigital41@gmail.com>
2025-11-05 09:24:05 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,5 @@ echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf
 # Aguarda 2 segundos para evitar problemas de timing no DNS
 sleep 2
 
-# Teste básico de resolução DNS antes de iniciar o app (opcional, mas útil para debug)
-ping -c 1 api.d4sign.com.br >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "[AVISO] DNS ainda não resolveu api.d4sign.com.br — continuando mesmo assim."
-else
-  echo "[OK] DNS está resolvendo normalmente."
-fi
-
 # Executa o comando principal do container
 exec "$@"
