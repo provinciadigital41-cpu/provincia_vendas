7af7e3563f02a0ba4031b4d50c51f58d52855f3c
provinciadigital41-cpu <provinciadigital41@gmail.com>
2025-11-11 11:34:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -436,8 +436,7 @@ function montarADDWord(d, nowInfo){
   const valorTotalNum = onlyNumberBR(d.valor_total);
   const parcelaNum = parseInt(String(d.parcelas||'1'),10)||1;
   const valorParcela = parcelaNum>0 ? valorTotalNum/parcelaNum : 0;
-  const marcasEspecForWord = String(d.marcas_espec || '')
-  .replace(/\r?\n/g, '\x0B');;
+  const marcasEspecForWord = String(d.marcas_espec || '').replace(/\r?\n/g, '\r\n');
 
   const valorPesquisa = 'R$ 00,00';
   const formaPesquisa = '---';
